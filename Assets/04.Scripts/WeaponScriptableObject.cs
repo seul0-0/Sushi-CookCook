40257ac07d546c4815edea419fc0528ad308d98e
bABCDd <rlatkdrbs6845@gmail.com>
2025-09-01 20:42:28 +0900

    [feat] 도구 상자UI제작, 도구 스크립터블 오브젝트 제작

---
+++ Assets/04.Scripts/WeaponScriptableObject.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "ScriptableObjects/Weapon")]
+public class WeaponScriptableObject : ScriptableObject
+{
+    public string ItemName;
+    public int ItemAttack;
+    public int CriticalChance;
+    public Image ItemImage;
+}
