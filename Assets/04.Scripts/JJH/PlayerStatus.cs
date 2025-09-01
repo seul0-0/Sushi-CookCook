e663a04bab7918509b9cb7aa816dfee3a853e363
jo-ji-hyun <newterr@naver.com>
2025-09-01 14:30:02 +0900

    [feat]업그레이드 수치 증가량

--- Assets/04.Scripts/JJH/PlayerStatus.cs
+++ Assets/04.Scripts/JJH/PlayerStatus.cs
@@ -12,4 +12,24 @@ public class PlayerStatus : ScriptableObject
     [Header("recipt")]                     // === 결산 ===
     public int money;
     public int luck;
+     
+    [Header("upgrade")]                    // === 업그레이드 수치 ===
+    public int attackUpgrade;
+    public int criticalUpgrade;
+    public int luckUpgrade;
+
+    public int UpgradeAttackValue(int bonus)
+    {
+        return attack + (attackUpgrade * bonus);
+    }
+
+    public int UpgradeCriticalValue(int bonus) 
+    {
+        return critical + (criticalUpgrade * bonus);
+    }
+
+    public int UpgradeLuckValue(int bonus)
+    {
+        return luck + (luckUpgrade * bonus);
+    }
 }
