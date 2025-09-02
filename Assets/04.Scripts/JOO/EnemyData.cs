a57494bbbed5df034ff0594866d8e2769ac5f0ac
슬기 <jsg8369@naver.com>
2025-09-02 20:18:19 +0900

    [feat]애너미 이미지 추가

--- Assets/04.Scripts/JOO/EnemyData.cs
+++ Assets/04.Scripts/JOO/EnemyData.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Enemy Data", menuName = "ScriptableObjects/EnemyData")]
+[CreateAssetMenu(fileName = "EnemyData", menuName = "ScriptableObjects/EnemyData")]
 public class EnemyData : ScriptableObject
 {
     public string enemyName;
