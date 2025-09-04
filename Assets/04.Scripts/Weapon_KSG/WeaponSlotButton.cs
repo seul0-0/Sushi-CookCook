dc1ccb7d604ee36928494c6fcdb0352cafb57db7
jo-ji-hyun <newterr@naver.com>
2025-09-04 12:47:07 +0900

    [feat] 무기 버튼에 각각 스크립트 착용

---
+++ Assets/04.Scripts/Weapon_KSG/WeaponSlotButton.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WeaponSlotButton : MonoBehaviour
+{
+    public int index;
+
+    public Button upgrade;
+    public Button equip;
+
+
+}
