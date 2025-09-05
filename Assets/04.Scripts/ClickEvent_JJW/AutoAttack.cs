3d8f37b7ebbc83f4a30efa8c535e3aa6ad0cecd1
jinwoo <jinwoo8857@gmail.com>
2025-09-05 10:19:28 +0900

    [chore] : Canvas배치 수정

--- Assets/04.Scripts/ClickEvent_JJW/AutoAttack.cs
+++ Assets/04.Scripts/ClickEvent_JJW/AutoAttack.cs
@@ -6,19 +6,27 @@ public class AutoAttack : MonoBehaviour
 {
     public Toggle autoAttackToggle;
     private Coroutine autoAttackRoutine;
-
+    public EnemyStatusTest enemyStatusTest;
     private PlayerStatus _playerStatus;
 
     void Start()
     {
         // 토글 이벤트 연결
         autoAttackToggle.onValueChanged.AddListener(OnToggleChanged);
-
+        EventManager.autoAttackClick += OnAttack;
         // StatusManager에서 현재 플레이어 상태 가져오기
         if (StatusManager.Instance != null)
         {
             _playerStatus = StatusManager.Instance.currentStatus;
         }
+    }
+    private void OnAttack() // Player 클릭을 받아들인 후 처리
+    {
+        float damage = CalculateFinalAttack();
+        enemyStatusTest.TakeDamage(damage);
+        Debug.Log("플레이어 공격력: " + damage);
+
+        // TODO: 여기서 EnemyManager 같은 곳으로 데미지를 전달하는 로직 필요
     }
 
     void OnToggleChanged(bool isOn)
@@ -64,7 +72,7 @@ public class AutoAttack : MonoBehaviour
             float attackInterval = Mathf.Max(0.5f, 2f - _playerStatus.stats[(int)StatType.autoattack].level * 0.2f);
             yield return new WaitForSeconds(attackInterval);
 
-            EventManager.attackClick?.Invoke();
+            EventManager.autoAttackClick?.Invoke();
             Debug.Log("자동공격 실행");
         }
     }
